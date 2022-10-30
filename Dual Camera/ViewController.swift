c58b607cc5e29fdc05bc84b643fc805c0795a914
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-10-30 17:08:07 +0100

    Initial Commit

---
+++ Dual Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dual Camera
+//
+//  Created by Aitor Zubizarreta on 30/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
