3665da8da0e6f91a7109263bc625a13fb408687f
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-10-30 17:26:05 +0100

    Deleted Main.storyboard and ViewController.swift files. Created the view controller "CamerasViewController" and its xib file. Configured the project (SceneDelegate) to display the new view controller's xib.

---
+++ Dual Camera/Views/CamerasViewController/CamerasViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CamerasViewController.swift
+//  Dual Camera
+//
+//  Created by Aitor Zubizarreta on 30/10/22.
+//
+
+import UIKit
+
+class CamerasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
